f4f56bc8f887964441f4b3eca794d151de1ca66e
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2025-08-23 14:08:56 +0300

    Refactor InventoryContainer to use promise-based image loading for background size calculation

--- src/client/components/InventoryContainer.tsx
+++ src/client/components/InventoryContainer.tsx
@@ -10,13 +10,14 @@ export default function InventoryContainer(O: {
     className?: string,
     children: ReactNode
 }) {
-    return <div className={O.className} ref={async el => {
+    return <div className={O.className} ref={el => {
         if (!el) return
         const img = Texture.get(O.src);
-        await img.wait();
-        const bb = img.getOpaqueBoundaries();
-        const width = bb.right - bb.left;
-        const height = bb.bottom - bb.top;
-        el.style.backgroundSize = `calc(${100 * img.width / width}%) calc(${100 * img.height / height}%)`;
+        img.wait().then(() => {
+            const bb = img.getOpaqueBoundaries();
+            const width = bb.right - bb.left;
+            const height = bb.bottom - bb.top;
+            el.style.backgroundSize = `calc(${100 * img.width / width}%) calc(${100 * img.height / height}%)`;
+        });
     }} style={O.containerState[0] === O.containerId ? {scale: "1"} : {}}>{O.children}</div>;
 }
